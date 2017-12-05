aababab5c8f76c9cff7a76b5fb5822976550d32e
Chris Daley <chebizarro@gmail.com>
2017-12-05 06:51:04 -0800

    moved LinkedList to Utils module

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["WaylandServer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mna/Networking.git", from: "0.2.0"),
+        .package(url: "https://github.com/cyllene-project/Networking.git", from: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
