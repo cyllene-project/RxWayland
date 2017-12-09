aef6f3053874627305703e9399bab4050b58d18b
Chris Daley <chebizarro@gmail.com>
2017-12-09 04:56:30 -0800

    added Apache-2.0 license and headers

--- Sources/Scanner/CodeWriter.swift
+++ Sources/Scanner/CodeWriter.swift
@@ -1,3 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Cyllene open source project
+//
+// Copyright (c) 2017 Chris Daley
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+//
+//===----------------------------------------------------------------------===//
+
 class CodeWriter {
 
 
