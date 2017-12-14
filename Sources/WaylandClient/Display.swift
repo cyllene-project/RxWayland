7edf88429d9982a32fc381a38f072174668abeb8
Chris Daley <chebizarro@gmail.com>
2017-12-13 21:45:30 -0800

    implementation of client Display

--- Sources/WaylandClient/Display.swift
+++ Sources/WaylandClient/Display.swift
@@ -26,10 +26,73 @@ public class Display {
 	var defaultQueue: EventQueue
 	var mutex: PThreadMutex
 
-	var readerCount: Int
+	var readerCount: Int = 0
 	var readSerial: UInt32
 	var readerCond: PThreadCond
 	
+	public static func connect(name:String?) throws -> Display {
+		
+		var fd: Int32
+		
+		if let connection = getenv("WAYLAND_SOCKET") {
+			
+			guard fd = Int32(connection, radix:10)
+			else throw DisplayError.invalidSocket(socket: connection)
+			
+			let flags = fcntl(fd, F_GETFD)
+			
+			if flags != -1 {
+				fcntl(fd, F_SETFD, flags | FD_CLOEXEC)
+			}
+			
+			unsetenv("WAYLAND_SOCKET")
+			
+		} else {
+
+			try fd = connectToSocket(name: name)
+		}
+
+		return connectToFd(fd:fd)
+		
+	}
+	
+	public static func connectToSocket(name: String?) throws -> Int32 {
+
+		guard let runtimeDir = getenv("XDG_RUNTIME_DIR")
+		else throw DisplayError.xdgDirNotSet
+		
+		let dName = name {
+			let dName =  getenv("WAYLAND_DISPLAY") {
+				dName = "wayland-0"
+			}
+		}
+		
+		let fd = socketCloexec(PF_LOCAL, SOCK_STREAM, 0)
+		
+		let addr = sockaddr_un()
+		
+		addr.sun_family = AF_LOCAL
+		
+		
+
+	}
+
+	public static func connectToFd(fd:Int32) throws -> Display {
+	
+		if let debug = getenv("WAYLAND_DEBUG") {
+			if debug == "client" || debug == "1" debugClient = true
+		}
+	
+		return Display(fd)
+	} 
+	
+	public func disconnect() {
+		
+		//free resources
+		
+		close(self.fd)
+		
+	}
 	
 	func wakeupThreads() {
 		readSerial++
@@ -91,5 +154,6 @@ public class Display {
 enum DisplayError {
 	case invalidObject = 0
 	case invalidMethod = 1
-	case noMemory = 2
+	case xdgDirNotSet = 2
+	case invalidSocket(socket: String)
 }
